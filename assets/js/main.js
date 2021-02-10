d7ca9a4414dd19a326e77df06f7fb20b702c7ece
yousefsaqoul <76205108+yousefsaqoul@users.noreply.github.com>
2021-02-10 16:07:31 +0100

    addAufgabe

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,25 @@ function youAge(){
 
 } 
 
+// ----------------------------------------------------------------
+
+
+
+function myResult() {
+    const nummer1 = document.getElementById("nummer1") //erste input
+    const nummer2 = document.getElementById("nummer2") //zweite input
+    const ergebnisse = document.getElementById("result22") //leer h1 um ergebnisse zu zeigen
+
+
+    if (nummer1.value == nummer2.value) {
+        const letzteResult = Number(nummer1.value) + Number(nummer2.value)* 5
+        ergebnisse.innerHTML = letzteResult;
+    } else if (nummer1.value !== nummer2.value) {
+        const letzteResult = Number(nummer1.value) + Number(nummer2.value) 
+        ergebnisse.innerHTML = letzteResult;
+    }
+}
+
 
 // const letzteNachricht = document.getElementById('myspan')
 // const Nachricht = document.getElementById('myNachricht')
