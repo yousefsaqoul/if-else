b12cbf1c9d35a7dbcbbccc6bf1b1e91caf15cd2f
yousefsaqoul <76205108+yousefsaqoul@users.noreply.github.com>
2021-02-10 14:21:41 +0100

    if-else

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+
+
+// age age yousef geschrieben
+const myage = document.getElementById('vol')
+function youAge(){
+    if (myage.value >= 18 ){
+        document.getElementById('yourAge').innerHTML = 'ja du kansst Shisha rauchenlcome'
+    } else { document.getElementById('yourAge').innerHTML = 'nein du darfst kein Shisha rauche'}
+
+} 
+
+
+// const letzteNachricht = document.getElementById('myspan')
+// const Nachricht = document.getElementById('myNachricht')
+
+// function myNachricht() {
+//     if (Nachricht.value == true) { letzteNachricht.innerHTMl = myNachricht.value
+
+//     }
+// }
